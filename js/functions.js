54e91a037e2320c6167bc8d4d3f84bfdfdfd7e3e
rafaela <rafaela.breed@luminis.eu>
2018-09-26 17:43:23 +0200

    Opvragen BPR & versturen naamswijziging

--- js/functions.js
+++ js/functions.js
@@ -5,6 +5,24 @@ jQuery(function($){
   $("#naam-keuze input.submit").click(function(){
 		$('#naam-keuze').toggleClass('hidden');
 		$('.afterLogin .modal').toggleClass('hidden');
+    //get personal data
+    $.ajax({
+      url: 'https://testen.solviteers.nl/MijnApp/api/v1/personen/950051998/wijzignaamgebruik?naamgebruik='+$('.form-group input[type=radio]:checked').val(),
+      type: 'POST',
+      dataType: 'json',
+      success: function (json) {
+        console.log(json);
+      },
+      error: function () {
+        console.log('boo!');
+      },
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization',
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDExMTk5MTAsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NTM5MjUvIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo1MzkyNS8ifQ.lxlaXPg7V8aMT5ViLanqUH6kBmCHx_VGN0oQ9wgNvUg');
+      }
+    });
+
+		$('#selected-result').text($('.form-group input[type=radio]:checked').val());
     return false;
 	});
 
